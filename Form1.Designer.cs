485c44cb035a7fc4656494bebab47c057870c7ce
numanakoluk <numanakoluk01@gmail.com>
2021-09-14 15:57:53 +0300

    Form Design Student And Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,61 @@ namespace StudentGradeRegistrationSystem
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STUDENT NO:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(236, 118);
+            this.maskedTextBox1.Mask = "0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(150, 30);
+            this.maskedTextBox1.TabIndex = 1;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(517, 450);
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(551, 362);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Student Grade Registration System";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ FrmStudentDetail.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace StudentGradeRegistrationSystem
+{
+    partial class FrmStudentDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblExam1 = new System.Windows.Forms.Label();
+            this.LblNameSurname = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblExam3 = new System.Windows.Forms.Label();
+            this.LblExam2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LblNo = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LblAvarage = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.LblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblExam1
+            // 
+            this.LblExam1.AutoSize = true;
+            this.LblExam1.Location = new System.Drawing.Point(200, 133);
+            this.LblExam1.Name = "LblExam1";
+            this.LblExam1.Size = new System.Drawing.Size(34, 24);
+            this.LblExam1.TabIndex = 0;
+            this.LblExam1.Text = "00";
+            // 
+            // LblNameSurname
+            // 
+            this.LblNameSurname.AutoSize = true;
+            this.LblNameSurname.Location = new System.Drawing.Point(200, 33);
+            this.LblNameSurname.Name = "LblNameSurname";
+            this.LblNameSurname.Size = new System.Drawing.Size(64, 24);
+            this.LblNameSurname.TabIndex = 1;
+            this.LblNameSurname.Text = "NULL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Exam 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Exam 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Exam 3:";
+            // 
+            // LblExam3
+            // 
+            this.LblExam3.AutoSize = true;
+            this.LblExam3.Location = new System.Drawing.Point(198, 239);
+            this.LblExam3.Name = "LblExam3";
+            this.LblExam3.Size = new System.Drawing.Size(34, 24);
+            this.LblExam3.TabIndex = 5;
+            this.LblExam3.Text = "00";
+            // 
+            // LblExam2
+            // 
+            this.LblExam2.AutoSize = true;
+            this.LblExam2.Location = new System.Drawing.Point(200, 185);
+            this.LblExam2.Name = "LblExam2";
+            this.LblExam2.Size = new System.Drawing.Size(34, 24);
+            this.LblExam2.TabIndex = 6;
+            this.LblExam2.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(155, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Name Surname:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(128, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 24);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "NO:";
+            // 
+            // LblNo
+            // 
+            this.LblNo.AutoSize = true;
+            this.LblNo.Location = new System.Drawing.Point(200, 80);
+            this.LblNo.Name = "LblNo";
+            this.LblNo.Size = new System.Drawing.Size(58, 24);
+            this.LblNo.TabIndex = 9;
+            this.LblNo.Text = "0000";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(72, 300);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(110, 24);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "AVARAGE:";
+            // 
+            // LblAvarage
+            // 
+            this.LblAvarage.AutoSize = true;
+            this.LblAvarage.Location = new System.Drawing.Point(198, 300);
+            this.LblAvarage.Name = "LblAvarage";
+            this.LblAvarage.Size = new System.Drawing.Size(34, 24);
+            this.LblAvarage.TabIndex = 11;
+            this.LblAvarage.Text = "00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(84, 354);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(90, 24);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "STATUS:";
+            // 
+            // LblStatus
+            // 
+            this.LblStatus.AutoSize = true;
+            this.LblStatus.Location = new System.Drawing.Point(194, 354);
+            this.LblStatus.Name = "LblStatus";
+            this.LblStatus.Size = new System.Drawing.Size(64, 24);
+            this.LblStatus.TabIndex = 13;
+            this.LblStatus.Text = "NULL";
+            // 
+            // FrmStudentDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(591, 473);
+            this.Controls.Add(this.LblStatus);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.LblAvarage);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.LblNo);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.LblExam2);
+            this.Controls.Add(this.LblExam3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblNameSurname);
+            this.Controls.Add(this.LblExam1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmStudentDetail";
+            this.Text = "STUDENT GRADE REGISTRATION SYSTEM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblExam1;
+        private System.Windows.Forms.Label LblNameSurname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblExam3;
+        private System.Windows.Forms.Label LblExam2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LblNo;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label LblAvarage;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label LblStatus;
+    }
+}
